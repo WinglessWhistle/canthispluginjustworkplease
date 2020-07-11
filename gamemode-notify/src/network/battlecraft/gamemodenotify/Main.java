9a75966e00a4a76a64048c64b38697438429e0a9
WinglessWhistle <58287456+WinglessWhistle@users.noreply.github.com>
2020-07-11 15:28:39 +1000

    thing

---
+++ gamemode-notify/src/network/battlecraft/gamemodenotify/Main.java
@@ -0,0 +1,25 @@
+package network.battlecraft.gamemodenotify;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		//server go brrrrrr
+	}
+	
+	@Override
+	public void onDisable() {
+		//server not got brrrrrrr
+	}
+	
+
+
+	public class MyListener implements Listener
+	{
+
+	}
+	
+}
